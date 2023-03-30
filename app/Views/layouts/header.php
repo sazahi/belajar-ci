e19b1a40ca571b80d0fdf44beeeca86775704380
sazahi <103054130+sazahi@users.noreply.github.com>
2023-03-30 14:09:42 +0700

    first commit

---
+++ app/Views/layouts/header.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1 class="mt-5 ">Welcome to Belajar Bareng SAZAHI</h1>
+    <p class="lead mb-5">Which is more difficult, coding or counting? Not both of them, the difficult one is sharing your knowledge with people without asking for the payment. <br/> <small>- Sazahi</small></p>
+    <p>Kunjungi blog belajar bareng SAZAHI dengan akses gratis <a href="https://SAZAHI.xyz ">disini</a>.</p>
+</div>
